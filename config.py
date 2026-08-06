6b3a302d479f45b1faeed2e5af074ba88f8c2619
galangrambu03 <galangciko86@gmail.com>
2026-08-06 20:48:02 +0700

    update

--- config.py
+++ config.py
@@ -34,3 +34,4 @@ class Config:
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", MAIL_USERNAME)
+    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL", "false").lower() == "true"
